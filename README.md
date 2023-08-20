7e8b2f1b02979d065ba4bf4cbe42aad79b7f2428
Kawamura Shingo <pannakoota@gmail.com>
2023-08-20 18:52:32 +0900

    main

---
+++ README.md
@@ -0,0 +1,73 @@
+# IP Address Assignment Script
+
+This script assigns unused IP addresses from specified CIDR subnets to a list of FQDNs.
+
+## Features
+
+1. **Expand FQDNs**: Converts range-based FQDNs into individual entries. E.g., `web[01-03].example.com` becomes:
+    ```
+    web01.example.com
+    web02.example.com
+    web03.example.com
+    ```
+
+2. **Generate IPs**: Generates a list of IP addresses for the given CIDR subnet. Currently, it supports `/24`, `/23`, and `/22` CIDR notation. 
+
+3. **Check Unused IPs**: For each FQDN, the script attempts to find an unused IP by checking DNS and pinging the IP.
+
+## Usage
+
+To use the script, you'll need to provide a list of FQDNs and a list of CIDR subnets. You can set these directly in the script before executing.
+
+### Step-by-Step:
+
+1. Open the script using a text editor.
+
+2. Locate the `fqdns` variable and set it to your list of FQDNs. Use line breaks for multiple entries. If you have range-based FQDNs, you can use the format like `web[01-03].example.com`.
+
+   **Example:**
+   ```bash
+   fqdns="web[01-03].example.com
+   db01.example.com
+   db02.example.com"
+   ```
+
+   This would represent the FQDNs:
+   - web01.example.com
+   - web02.example.com
+   - web03.example.com
+   - db01.example.com
+   - db02.example.com
+
+3. Locate the `subnets` variable and set it to your list of CIDR subnets. Use line breaks for multiple entries.
+
+   **Example:**
+   ```bash
+   subnets="192.168.1.0/24
+   192.168.2.0/24"
+   ```
+
+   This would represent the CIDR subnets:
+   - 192.168.1.0/24
+   - 192.168.2.0/24
+
+4. Save the changes and close the text editor.
+
+5. Execute the script:
+
+```bash
+./assign_ips.sh
+```
+
+Output:
+```
+web01.example.com,192.168.1.1
+web02.example.com,192.168.1.2
+...
+```
+
+## Notes
+
+- The script uses both `host` and `ping` commands to verify if an IP address is unused. Ensure that these commands are available on your system.
+- The script currently supports `/24`, `/23`, and `/22` CIDR notations. If you need more CIDR notations, you can extend the `generate_ips` function.
+
---
+++ assign_ips.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Function to expand fqdn
+expand_fqdn() {
+    local input="$1"
+    local final_output=""
+    # Handle multi-line input
+    while IFS= read -r line; do
+        local temp_input="$line"
+        while [[ $temp_input =~ \[([0-9]+)-([0-9]+)\] ]]; do
+            local prefix="${temp_input%%\[*\]*}"
+            local middle="${BASH_REMATCH[1]}-${BASH_REMATCH[2]}"
+            local suffix="${temp_input#*\]}"
+            local expanded=""
+            IFS=- read start end <<< "$middle"
+            for num in $(seq -w $start $end); do
+                expanded+="${prefix}${num}${suffix}\n"
+            done
+            temp_input=$expanded
+        done
+        final_output+="$temp_input"
+    done <<< "$input"
+    echo -e "$final_output"
+}
+
+
+# Function to generate IPs for given subnet
+generate_ips() {
+    local subnet=$1
+#    echo "Debug: Processing subnet: $subnet" >&2  # ????????????
+    IFS='/' read -ra ADDR <<< "$subnet"
+    local ip_parts=($(echo ${ADDR[0]} | awk -F. '{print $1" "$2" "$3" "$4}'))
+    local cidr=${ADDR[1]}
+    local ip_end=240 # ip range 1-240 
+    case $cidr in
+        24)
+            for i in $(seq 1 $ip_end); do
+                echo "${ip_parts[0]}.${ip_parts[1]}.${ip_parts[2]}.$i"
+            done
+            ;;
+        23)
+            for third in $(seq ${ip_parts[2]} $((${ip_parts[2]}+1))); do
+                for i in $(seq 1 $ip_end); do
+                    echo "${ip_parts[0]}.${ip_parts[1]}.$third.$i"
+                done
+            done
+            ;;
+        22)
+            for third in $(seq ${ip_parts[2]} $((${ip_parts[2]}+3))); do
+                for i in $(seq 1 $ip_end); do
+                    echo "${ip_parts[0]}.${ip_parts[1]}.$third.$i"
+                done
+            done
+            ;;
+        
+        # CIDR 21 20 ...
+        #
+
+        *)
+            echo "CIDR $cidr is not currently supported."
+            ;;
+    esac
+}
+
+# Expand and remove duplicate FQDNs
+expanded_fqdns=$(expand_fqdn "$fqdns")
+fqdns=$(echo "$expanded_fqdns" | sort -u | sed '/^$/d')
+
+# Remove leading and trailing whitespaces and any unwanted newlines
+subnets=$(echo "$subnets" | sed 's/^[ \t]*//;s/[ \t]*$//;/^$/d')
+
+# Array to store used IP addresses
+declare -A used_ips
+declare -A assigned_fqdns
+
+# Find unused IP addresses and assign to FQDNs
+while IFS= read -r fqdn; do
+
+    # Skip this fqdn if already assigned
+    [ "${assigned_fqdns[$fqdn]}" == "1" ] && continue
+    while IFS= read -r subnet; do
+        while IFS= read -r ip; do
+            if [ -z "${used_ips[$ip]}" ]; then
+
+                # Background process to check DNS and ping
+                ( ! host $ip > /dev/null 2>&1 && ! ping -c 1 -W 1 $ip > /dev/null 2>&1 ) &
+
+                # Wait for background process
+                wait $!
+
+                # If the background process was successful
+                if [ $? -eq 0 ]; then
+                    echo "$fqdn,$ip"
+                    used_ips[$ip]=1       # Mark this IP address as used
+                    assigned_fqdns[$fqdn]=1 # Mark this FQDN as assigned
+                    break
+                fi
+            fi
+        done < <(generate_ips "$subnet")
+
+        # If the fqdn has been assigned an IP, break out of the subnet loop
+        [ "${assigned_fqdns[$fqdn]}" == "1" ] && break
+    done <<< "$subnets"
+done <<< "$fqdns"
